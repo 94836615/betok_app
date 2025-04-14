7edcae4d7adeeff68789d0727fa8d89ce0c04a5f
Noah Nap <s1172260@student.windesheim.nl>
2025-04-15 01:29:08 +0200

    refactor(analytics): implemented analytics tools

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/navigation/Navigation.tsx';
+import { setJSExceptionHandler } from 'react-native-exception-handler';
 
 function App(): React.JSX.Element {
+  // Set up global error handling
+  setJSExceptionHandler((error, _isFatal) => {
+    console.log(`Caught JS Error: ${error.name} ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    // Log to a service like Crashlytics
+  }, true);
 
   return (
     <NavigationContainer>
@@ -10,6 +17,5 @@ function App(): React.JSX.Element {
     </NavigationContainer>
   );
 }
-
 
 export default App;
